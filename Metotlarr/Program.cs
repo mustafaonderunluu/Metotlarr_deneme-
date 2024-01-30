b552b0d0998e81e2b3bb0335def2e89dc39e1e00
Mustafa Önder ÜNLÜ <116373504+mustafaonderunluu@users.noreply.github.com>
2024-01-30 13:35:58 +0300

    Proje dosyası ekle.

---
+++ Metotlarr/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Metotlarr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Temizle();
+        }
+        
+        public static void Temizle()
+        {
+            while (true)
+            {
+                Console.WriteLine("Bir değer girin veya 'exit' yazarak ayrılın ");
+
+                string değer =Console.ReadLine();
+
+                if (değer == "exit")
+                {
+                    Console.WriteLine("ayrıldınız");
+                    break;
+                }
+
+                else if(değer=="onder" )
+                {
+                    Console.WriteLine("Doğru");
+
+
+                }
+                else {
+                    Console.WriteLine("Geçersiz işlem");
+                }
+
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
